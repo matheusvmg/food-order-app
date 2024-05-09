0bbaf79d1b9dcdd6c806ee2b891e9d33b058b7f4
Matheus Vinícius <matheusvmg@gmail.com>
2024-05-09 20:53:43 -0300

    Register all products options and order routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import { Cors } from "./middlewares/Cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { CategoriesRoutes } from "./routes/Categories/CategoriesRoutes";
+import { ProductsOptionsRoutes } from "./routes/ProductsOptions/ProductsOptions";
+import { OrderRoutes } from "./routes/Order/OrderRoutes";
 
 class App {
   app = express();
@@ -14,7 +16,9 @@ class App {
   productsRoutes = new ProductsRoutes(this.app);
   userRoutes = new UserRoutes(this.app);
   categoriesRoutes = new CategoriesRoutes(this.app);
-  applicationServer = new ApplicationServer(this.app);
+  productsOptionsRoutes = new ProductsOptionsRoutes(this.app);
+  orderRoutes = new OrderRoutes(this.app);
+  applicationServer = new ApplicationServer(this.http, this.io);
 
   initialize() {
     this.applicationServer.start();
@@ -23,6 +27,9 @@ class App {
     this.productsRoutes.registerAllRoutes();
     this.userRoutes.registerAllRoutes();
     this.categoriesRoutes.registerAllRoutes();
+    this.productsOptionsRoutes.registerAllRoutes();
+    this.orderRoutes.registerAllRoutes();
+    this.io.on("connection", () => console.log("Client Connected ✅"));
   }
 }
 
